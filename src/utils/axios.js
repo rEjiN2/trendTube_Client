a2db250af9159f965da61a4e5f7d3640aca48d49
rEjiN2 <rejin374@gmail.com>
2023-05-30 15:18:28 +0530

    urlnew

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ axios.defaults.withCredentials = true;
 
 const instance = axios.create({
     
-    baseURL:'http://16.16.167.249/api/'
+    baseURL:'https://api.trendtube.online/api/'
 })
 
 export default instance
